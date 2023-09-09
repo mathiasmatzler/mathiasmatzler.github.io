e0f031f5f06bc0868db6f1217ba0f7fc904b0825
Lukas <alberlukas@live.de>
2023-09-09 20:31:48 +0200

    Adding feature request post contract..

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,13 @@
+import FileSaver from 'file-saver';
+import Typed from 'typed.js';
+import jQuery from "jquery";
+
+window.$ = window.jQuery = jQuery;
+import paper, {Path} from 'paper'
+
 $(document).ready(function () {
+  // check if mobile
+  var isMobile = $(window).width() < 600;
 
   // start overlay infomation text
   new Typed('#startOverlay>p', {
@@ -7,8 +16,11 @@ $(document).ready(function () {
   });
   // variables for drawing
   const imageGrid = $('#grid');
-  const canvas = document.getElementById('myCanvas');
+  const canvasBottom = document.getElementById('myCanvas');
+  const canvasTop = document.getElementById('topCanvas');
   var path;
+  var path2;
+  var path2_drawn;
 
 
   // Fetch the subfolders in the "images" folder
@@ -49,56 +61,103 @@ $(document).ready(function () {
     });
     //after loading do...
     // init canvas dimensions
-    canvas.width = imageGrid.width();
-    canvas.height = imageGrid.height();
-    console.log("canvas: ", canvas.width, canvas.height);
+    canvasBottom.width = canvasTop.width = imageGrid.width();
+    canvasBottom.height = canvasTop.height = imageGrid.height();
+    console.log("canvas: ", canvasBottom.width, canvasBottom.height);
 
     // Calculate center of container
     var winCenterX = window.innerWidth / 2;
     var winCenterY = window.innerHeight / 2;
 
     // Init previous mouse position
-    var prevMouseX = 220;
-    var prevMouseY = 220;
+    var prevMouseX = isMobile ? 160 : 220;
+    var prevMouseY = isMobile ? 120 : 220;
 
-    // initialize Paper.js
-    // paper.install(window); // Install Paper.js on the global scope
-    paper.setup(canvas);
+    // initialize Paper.js - create different scopes for each canvas
+    const scopeBackground = new paper.PaperScope();
+    const scopeTop = new paper.PaperScope();
+    scopeBackground.settings.insertItems = false; // prevents automatic insertion of items into the DOM
+    scopeTop.settings.insertItems = false; // prevents automatic insertion of items into the DOM
+    scopeBackground.setup(canvasBottom);
+    scopeTop.setup(canvasTop);
+    const layerBackgroud = scopeBackground.project.activeLayer;
+    const layerTop = scopeTop.project.activeLayer;
 
     // readjust canvas dimensions
-    canvas.width = imageGrid.width();
-    canvas.height = imageGrid.height();
+    canvasBottom.width = canvasTop.width = imageGrid.width();
+    canvasBottom.height = canvasTop.height = imageGrid.height();
     // console.log("canvas: " , canvas.width, canvas.height );
 
     // Create a white background rectangle covering the canvas
-    var background = new paper.Path.Rectangle(paper.view.bounds);
+    var background = new scopeBackground.Path.Rectangle(scopeBackground.view.bounds);
     background.fillColor = 'white'; // Set the background color to white
+    layerBackgroud.addChild(background);
+
+    // Create a mask
+    let blackBackground = new scopeTop.Path.Rectangle(
+      new scopeTop.Point(-50000, -50000),
+      new scopeTop.Size(100000, 100000)
+    );
+    let mask = new scopeTop.Path.Rectangle(
+      new scopeTop.Point(-30, -30),
+      new scopeTop.Size(500, 500)
+    );
+    // let debug = new scopeTop.Path.Rectangle(
+    //   new scopeTop.Point(0, 0),
+    //   new scopeTop.Size(500, 500)
+    // );
+    const invertedMask = blackBackground.subtract(mask);
+    invertedMask.fillColor = 'black';
+    invertedMask.clipMask = true;
+    layerTop.addChild(invertedMask);
+    // debug.strokeColor = 'red';
+    // debug.strokeWidth = 1;
+    // layerTop.addChild(debug);
 
     function updatePosition(event) {
       // Calculate the mouse position relative to the container
       var mouseX = prevMouseX + event.movementX * 2; // *-1 to invert mouse movement
       var mouseY = prevMouseY + event.movementY * 2; // *-1 to invert mouse movement
 
-      // console.log("mouseX: " + mouseX);
-      // console.log("mouseY: " + mouseY);
+      console.log("mouseX: " + mouseX);
+      console.log("mouseY: " + mouseY);
 
       $('#myCanvas').css({
+        'transform': 'translate(' + ((mouseX - winCenterX) * -1) + 'px, ' + ((mouseY - winCenterY) * -1) + 'px)' // *-1 to invert movement of container so that it moves in the opposite direction of the mouse
+      });
+      $('#topCanvas').css({
         'transform': 'translate(' + ((mouseX - winCenterX) * -1) + 'px, ' + ((mouseY - winCenterY) * -1) + 'px)' // *-1 to invert movement of container so that it moves in the opposite direction of the mouse
       });
       imageGrid.css({
         'transform': 'translate(' + ((mouseX - winCenterX) * -1) + 'px, ' + ((mouseY - winCenterY) * -1) + 'px)' // *-1 to invert mouse movement so that it moves in the opposite direction of the mouse
       });
+      invertedMask.translate(new scopeTop.Point(event.movementX * 2, event.movementY * 2));
+      // debug.translate(new scopeTop.Point(event.movementX * 2, event.movementY * 2));
 
       if (!path) {
-        path = new paper.Path();
-        path.strokeColor = '#DDDDDD'; // Adjust the color as desired
-        path.strokeWidth = 5; // Adjust the stroke width as desired
-        path.strokeCap = 'round'; // Adjust the stroke cap as desired
+        path = new scopeBackground.Path();
+        path2 = new scopeTop.Path();
+        path2_drawn = new scopeTop.Path();
+        path2.strokeColor = path.strokeColor = '#DDDDDD'; // Adjust the color as desired
+        path2.strokeWidth = path.strokeWidth = 5; // Adjust the stroke width as desired
+        path2.strokeCap = path.strokeCap = 'round'; // Adjust the stroke cap as desired
+        layerBackgroud.addChild(path);
+        layerTop.addChild(path2);
+        // layerTop.addChild(path2_drawn);
       }
-      var point = new paper.Point(mouseX, mouseY);
+      var point = new scopeBackground.Point(mouseX, mouseY);
       path.add(point);
+      path2.add(point);
       path.smooth();
-      paper.view.draw();
+      path2.smooth();
+      invertedMask.bringToFront();
+      // scopeTop.project.
+      // const substract = path2.subtract(invertedMask);
+      // layerTop.removeChildren();
+      // layerTop.addChild(substract);
+
+      // let ctx = canvasTop.getContext("2d");
+      // ctx.clearRect(50, 50, 300, 300)
 
       // Update previous mouse position
       prevMouseX = mouseX;
@@ -118,22 +177,14 @@ $(document).ready(function () {
 
       // Convert the canvas to a data URL
       var dataURL = canvas.toDataURL('image/png');
-
-      // Create a temporary link element
-      var link = document.createElement('a');
-      link.href = dataURL;
-      link.download = 'your own line of research.png'; // Adjust the filename as desired
-
-      // Simulate a click on the link to trigger the download
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-
-      // last but not least reload the page
+      // call FileSaver.js
+      FileSaver.saveAs(dataURL, 'your own line of research.png');
     });
 
-    // ------------------ Pointer Lock ------------------ //
-    if ($(window).width() > 600) {
+    // implementation of pointer lock for desktop or touch for mobile
+    if (!isMobile) {
+
+      // ------------------ Pointer Lock ------------------ //
       // Lock the pointer when the user clicks on the canvas
       $('#startOverlay').click(() => {
         $('#myCanvas').get(0).requestPointerLock();
@@ -145,7 +196,7 @@ $(document).ready(function () {
       document.addEventListener("pointerlockchange", lockChangeAlert, false);
 
       function lockChangeAlert() {
-        if (document.pointerLockElement === canvas) {
+        if (document.pointerLockElement === canvasBottom) {
           console.log("The pointer lock status is now locked");
           document.addEventListener("mousemove", updatePosition, false);
           $('#startOverlay').hide();
@@ -163,17 +214,19 @@ $(document).ready(function () {
     } else {
       // ------------------ Touch Mobile ------------------ //
       $('#startOverlay').click(() => {
+        // update initial position
+        updatePosition({movementX: 0, movementY: 0});
+        // hide overlay, show spyhole, show escText, add event listeners
         entryMobile();
       });
-      // $('#spyhole').click(() => {
-      //   entryMobile();
-      // });
 
       var previousTouch;
+
       function entryMobile() {
         console.log("entryMobile");
         addEventListener("touchmove", e => {
           e.preventDefault(); // prevent scrolling when inside DIV (mobile)
+          e.stopImmediatePropagation();
           const touch = e.touches[0];
 
           if (previousTouch) {
@@ -181,14 +234,14 @@ $(document).ready(function () {
             e.movementX = (touch.pageX - previousTouch.pageX) * 2;
             e.movementY = (touch.pageY - previousTouch.pageY) * 2;
 
-            console.log("e.movementX: " + e.movementX);
-            console.log("e.movementY: " + e.movementY);
+            // console.log("e.movementX: " + e.movementX);
+            // console.log("e.movementY: " + e.movementY);
 
             updatePosition(e);
           }
           previousTouch = touch;
 
-        }, false);
+        }, {passive: false});
 
         addEventListener("touchend", (e) => {
           previousTouch = null;
